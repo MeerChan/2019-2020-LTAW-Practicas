9ff1d8848b1d7377b52047ae0c4a52bd19a64083
Isaac Merchan Blanco <imerchan@f-l3208-pc02.aulas.etsit.urjc.es>
2020-02-03 11:43:23 +0100

    server corto

--- P1/testarchivos.js
+++ P1/testarchivos.js
@@ -5,16 +5,8 @@ console.log("Arrancando servidor...")
 
 //-- Configurar el servidor. Cada vez que llegue una peteicion se emite un
 //-- evento y se invoa a la funcion server_req
-server = http.createServer(server_req)
-
-//-- Funcion de retrollamada de servicio de las peticiones
-//-- No se devuelve mensaje, se indica en consola que ha llegado
-//-- una peticion
-function server_req(req, res) {
+http.createServer((req,res) =>{
   console.log("---> Peticion recibida")
-}
-
-//-- Queremos que el servidor escuche peticiones en puerto 8080
-server.listen(PUERTO);
+}).listen(PUERTO);
 
 console.log("Puerto: " + PUERTO)

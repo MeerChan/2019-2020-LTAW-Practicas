576002be8a766c14b1810a34d513cab8d106534b
Isaac Merchan Blanco <imerchan@f-l3208-pc02.aulas.etsit.urjc.es>
2020-02-03 12:49:15 +0100

    lee archivo

--- P1/server.js
+++ P1/server.js
@@ -1,16 +1,32 @@
 //-- Puerto donde recibir las peticiones
 const PUERTO = 8080;
-
+const fs = require('fs');
 //-- Modulo http
 const http = require('http');
 
 console.log("Arrancando servidor...")
 
+
+//-- Leer el fichero. Al terminar se invoca a la función show_file
+
+
+//-- Funcion llamada cuando se ha terminado de leer el fichero
+function show_file(err, data) {
+    console.log("---> Comienzo del fichero leido")
+    console.log(data)
+    console.log("---> Final del fichero")
+}
+
 http.createServer((req,res) =>{
   console.log("---> Peticion recibida")
   console.log("Recurso solicitado (URL): " + req.url)
+  //-- me dice todas las propiedades de la cabecera
   for (prop in req.headers) {
     console.log("Propiedad: " + prop + " --> Valor: " + req.headers[prop])
+  }
+  if(req.url == "/producto1"){
+    fs.readFile('producto1.txt', 'utf8', show_file);
+    console.log("ENTRAAAAA")
   }
 }).listen(PUERTO);
 

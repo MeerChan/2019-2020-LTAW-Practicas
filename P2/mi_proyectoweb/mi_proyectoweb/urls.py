7b0114ec13adf82c002d0d3141ebfebb34f943c3
Isaac Merchan Blanco <imerchan@f-l3208-pc20.aulas.etsit.urjc.es>
2020-02-17 12:46:19 +0100

    prueba django

--- P2/mi_proyectoweb/mi_proyectoweb/urls.py
+++ P2/mi_proyectoweb/mi_proyectoweb/urls.py
@@ -13,15 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
 
-# -- Importar todas las vistas de mi_tienda
-from . import views
-
-# -- Aquí se definen las URLs de nuestra tienda
-# -- Metemos de momento sólo la principal (índice)
+# -- Recursos de mi proyecto web
 
 urlpatterns = [
-    # -- Vista pricipal (índice)
-    path('', views.index, name='index'),
+    # -- Esta es la URL de mi tienda
+    path('mi_tienda/', include('mi_tienda.urls')),
+
+    # -- URL de administracion (por defecto en django)
+    path('admin/', admin.site.urls),
 ]

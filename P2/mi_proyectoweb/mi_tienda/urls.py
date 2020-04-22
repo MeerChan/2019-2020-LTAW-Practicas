ff09e803fff7d1e54e3cd9b23a30ce957ced0083
imerchan <i.merchan.2016@alumnos.urjc.es>
2020-04-22 16:22:08 +0200

    tienda con plantillas

--- P2/mi_proyectoweb/mi_tienda/urls.py
+++ P2/mi_proyectoweb/mi_tienda/urls.py
@@ -10,8 +10,12 @@ urlpatterns = [
     # -- Vista pricipal (índice)
     path('', views.index, name='index'),
     path('test1/', views.test1, name='test1'),
-    path('test3/', views.test3, name='test2'),
+    path('test2/', views.test3, name='test2'),
     path('test3/', views.test3, name='test3'),
     path('test4/', views.test4, name='test4'),
     path('test5/', views.test5, name='test5'),
+    path('list/', views.list, name='list'),
+    path('goku/', views.goku, name='goku'),
+    path('vegeta/', views.vegeta, name='vegeta'),
+    path('frezer/', views.frezer, name='frezer'),
 ]
--- P2/mi_proyectoweb/mi_tienda/views.py
+++ P2/mi_proyectoweb/mi_tienda/views.py
@@ -3,12 +3,13 @@ from django.template import Template, Context
 from django.template.loader import get_template
 from django.shortcuts import render
 from random import randint
+from mi_tienda.models import Producto
 
 # -- Vista principal de mi tienda
 # -- El nombre de la vista puede ser cualquiera. Nosotros lo hemos
 # -- llamado index, pero se podría haber llamado pepito
 def index(request):
-    return HttpResponse("Hola! esta es la página principal de Mi tienda!")
+    return render(request, 'index.html')
 
 # -- Ejemplo de generacion a partir de cadenas con código html
 def test1(request):
@@ -93,3 +94,22 @@ def test5(request):
     # -- Obtener el número aleatorio
     numero = randint(0, 100)
     return render(request, 'test5.html', {'numero':str(numero)})
+def list(request):
+    productos = Producto.objects.all()
+    html = "<h2>Listado de articulos</h2>"
+    for prod in productos:
+        print(prod.nombre)
+        html += '<p>'+ prod.nombre + ' ' + str(prod.precio) + '<p>'
+    return HttpResponse(html)
+
+def goku(request):
+    productos = Producto.objects.all()
+    return render(request, 'goku.html', {'productos':productos[0]})
+
+def vegeta(request):
+    productos = Producto.objects.all()
+    return render(request, 'vegeta.html', {'productos':productos[1]})
+
+def frezer(request):
+    productos = Producto.objects.all()
+    return render(request, 'frezer.html', {'productos':productos[2]})
